ba8f135e66442b3f989bad4e5081abce2acc6210
mahmoud2227 <mahmoud.salah2227@yahoo.com>
2022-01-21 12:14:43 +0200

    initial code

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# udacityProject1
+##Udacity Project 1
+This is the first project for udacity web development professional track
+
+## Technologies
+This project made with:
+1-html
+2-css
+3-vanilla javascript
+
+## Features
+1- The page is resposive and works well in small and medium screens
+2- the navbar is dynamicly generated using javascript
+3- the section that the user read is highlighted
+4- the navbar allow the user to scroll to the contents of the page without the need of archor tag
+5- the navbar is hidden when the user isn't scrolling
---
+++ js/app.js
@@ -0,0 +1,95 @@
+/**
+ * Define Global Variables
+ *
+ */
+
+const navBarList = document.getElementById("navbar__list");
+const sections = document.querySelectorAll("section");
+const header = document.querySelector(".page__header");
+let isScrolling;
+
+/**
+ * End Global Variables
+ * Start Helper Functions
+ *
+ */
+
+// get the positon of the section
+function sectionPositon(element) {
+    let sectionPos = element.getBoundingClientRect();
+    return sectionPos;
+}
+
+/**
+ * End Helper Functions
+ * Begin Main Functions
+ *
+ */
+
+// create a listItem for every section made in the website
+function createListItem() {
+    for (let section of sections) {
+        const listItem = document.createElement("li");
+        listItem.innerHTML = `<a class="menu__link" href="#${section.id}">${section.getAttribute("data-nav")}</a>`;
+        navBarList.appendChild(listItem);
+    }
+}
+
+// Add class 'active' to section when near top of viewport
+function toggleSectionClass() {
+    sections.forEach((section) => {
+        // if (sectionPositon(section).top >= 0 && +sectionPositon(section).top < section.offsetHeight) {
+        //     sections.forEach((sec) => sec.classList.remove("your-active-class"));
+        //     section.classList.add("your-active-class");
+        // }
+        if (sectionPositon(section).top >= 0 && +sectionPositon(section).top < section.offsetHeight - 80) {
+            sections.forEach((sec) => sec.removeAttribute("class"));
+            section.classList.add("your-active-class");
+        }
+    });
+}
+
+// Scroll to anchor ID using scrollTO event
+function scrollToSection() {
+    header.addEventListener("click", function (event) {
+        event.preventDefault();
+        sections.forEach((section) => {
+            if (event.target.getAttribute("href").substring(1) === section.getAttribute("id")) {
+                window.scrollTo({
+                    top: section.offsetTop,
+                    left: 0,
+                    behavior: "smooth",
+                });
+            }
+        });
+    });
+}
+
+// Hide the header when the user isn't scrolling
+function NoScrollHideNav() {
+    window.addEventListener("scroll", function () {
+        // Clear our timeout throughout the scroll
+        window.clearTimeout(isScrolling);
+
+        // Set a timeout to run after scrolling ends
+        isScrolling = setTimeout(function () {
+            // Run the callback
+            header.classList.add("hidden");
+        }, 2000);
+        header.classList.remove("hidden");
+    });
+}
+/**
+ * End Main Functions
+ * Begin Events
+ *
+ */
+
+// Build menu
+createListItem();
+// Scroll to section on link click
+scrollToSection();
+// Set sections as active
+document.addEventListener("scroll", toggleSectionClass);
+// Hide the header on no scroll
+NoScrollHideNav();
